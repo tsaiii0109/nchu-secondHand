8c4a89eb82c9d0f6de06c7e89b2726ef59b6d837
SC1314520 <s112029013@smail.nchu.edu.tw>
2023-10-13 00:01:58 +0800

    debug

--- gs/reserve.js
+++ gs/reserve.js
@@ -9,15 +9,21 @@ function doPost(e) {
   var intro=param.intro;
   var key =param.key;
   try{
-    var html = '預約者：'+buyer+'<br>商品名稱：'+title+'<br>商品價格：'+price+'<br>商品介紹：'+intro+'<br>商品快照：'+img+'<br>賣家聯絡：'+seller;
+    var html = '預約者：'+buyer+'<br>商品名稱：'+title+'<br>商品價格：'+price+'<br>商品介紹：'+intro+'<br>賣家聯絡：'+seller;
     MailApp.sendEmail(seller,'預約通知函',{},{
       noReply:true,
       htmlBody:html,
-      cc:buyer
+      cc:buyer,
+      attachments:[createBlob(img)]
     })
     sheet.appendRow([key,seller,title,price,buyer]);
   }catch(e){
     return ContentService.createTextOutput('failed').setMimeType(ContentService.MimeType.TEXT);
   }
   return ContentService.createTextOutput('預約成功').setMimeType(ContentService.MimeType.TEXT);
+}
+function createBlob(base64){
+  var mimeType = base64.split(";")[0].split(":")[1];
+  var data= Utilities.base64Decode(base64.split(";")[1].split(",")[1]); 
+  return Utilities.newBlob(data,mimeType,'商品快照');
 }
